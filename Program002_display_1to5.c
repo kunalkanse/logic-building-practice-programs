47de3d6c0cdc840c9b96d777570fe02f941a0865
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-11-17 23:14:52 +0530

    Create Program002_display_1to5.c

---
+++ Program002_display_1to5.c
@@ -0,0 +1,30 @@
+// Display 1 to 5 on screen..
+
+
+// printing statements in different languages
+/*  c -> printf()
+    c++ -> cout<<
+    java - > System.out.println();
+    python -> print();
+    javasript -> console.log()
+    typescript -> console.log() 
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    printf("1\n");
+    printf("2\n");
+    printf("3\n");
+    printf("4\n");
+    printf("5\n");
+
+    return 0;   // return to OS
+}
+
+// To complile the code
+//  gcc Program005.c -o myexe
+
+//To run code
+// .\myexe
