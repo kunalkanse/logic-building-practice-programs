8a00b42d5675c2000b1a0cd2441f16c04c60802d
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-11-17 23:35:43 +0530

    Create Program003_1to5_loop.c

---
+++ Program003_1to5_loop.c
@@ -0,0 +1,21 @@
+
+// Display 1 to 5 using loop
+// perform Dry run
+
+#include<stdio.h>
+
+int main()
+{
+    int i = 0;
+
+    for(i = 1; i <= 5; i++)
+    {
+        printf("%d\n",i);
+    }
+
+    return 0;
+}
+
+// Starting point :  1
+// Ending point :  5
+// Displacement :  1
