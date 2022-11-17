17873f39f2e3f06eaaf26ee7bf731e49fed9d897
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-11-18 01:01:54 +0530

    Create Program009_Filter_Updater.c

---
+++ Program009_Filter_Updater.c
@@ -0,0 +1,47 @@
+
+// using filters & uodater for incorrect input
+
+#include<stdio.h>
+
+// Input : 3
+// Output : 1   2   3
+
+// Input : 7
+// Output : 1   2   3   4   5   6   7
+
+void Display(int);         // Declaration
+
+int main()
+{
+    int iNo= 0;
+    
+    printf("Enter number\n");
+    scanf("%d",&iNo);
+    
+    Display(iNo);      // Disaply(3);
+    
+    return 0;
+}
+
+void Display(int iValue)
+{
+    int i = 0;
+  
+    // if user enter 0
+    if(iValue == 0) //filter
+    {
+        printf("You entered 0");
+        return;
+    }
+    
+    // if user enter negative values
+    if(iValue < 0)  //updater
+    {
+        iValue = -iValue;
+    }
+    
+    for(i = 1; i <= iValue; i++)
+    {
+        printf("%d\n",i);
+    }
+}
