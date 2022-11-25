96feb6f6da710d3c496e945bc432fe1b1b33d071
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-11-25 11:43:20 +0530

    Create Program028_ReversePosNeg.c

---
+++ Program028_ReversePosNeg.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+// Reverse a number
+// works for positive and negative both numbers
+
+int Reverse(int);
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number : ");
+    scanf("%d",&iValue);
+
+    int iRet = Reverse(iValue);
+    printf("Reversed Number is : %d",iRet);
+
+    return 0;
+}
+
+int Reverse(int iNo)
+{
+    int iDigit = 0 , iRev = 0;
+
+    while(iNo != 0) // or (iNo != 0)
+    {
+        iDigit = iNo % 10;
+        iRev = (iRev * 10) + iDigit;    // To reverse number
+        iNo = iNo / 10;
+    }
+
+    return iRev;
+}
