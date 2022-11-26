4fca37551338c10c45b9131cde4e7cad14ca27e5
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-11-26 21:45:42 +0530

    Create Program049_Pattern_Even_Odd.c

---
+++ Program049_Pattern_Even_Odd.c
@@ -0,0 +1,50 @@
+
+ /*
+    iRow = 4
+    iCol = 4
+ 
+    *   *   *   *
+    #   #  #    #
+    *   *   *   *
+    #   #   #    #
+ */
+
+#include<stdio.h>
+
+void Display(int Row , int Col)
+{
+    int iCnt = 0;
+    int jCnt = 0;
+
+    for(iCnt = 1; iCnt <= Row ; iCnt++)
+    {
+        for (jCnt = 1; jCnt <= Col; jCnt++)
+        {
+            if((iCnt % 2) == 0)
+            {
+                printf("#\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+    
+    printf("Enter number of rows\n");
+    scanf("%d",&iValue1);
+    
+    printf("Enter number of columns\n");
+    scanf("%d",&iValue2);
+    
+    Display(iValue1, iValue2);//Display(3,3);
+    
+    return 0;
+}
