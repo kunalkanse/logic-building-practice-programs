b90a4cf92bc0ba7a2a06472d3c4bc305fa135735
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-11-27 02:05:49 +0530

    Create Program056_PassingArray_Pointer.c

---
+++ Program056_PassingArray_Pointer.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+// passing Array to the function, use pointer & return Addition of its elements
+
+//Array is internally considered as pointer
+
+//int Addition(int Brr[])
+int Addition(int *Brr)
+{
+    int iSum = 0;
+
+    for(int iCnt = 0; iCnt < 5 ; iCnt++)
+    {
+        iSum = iSum + (*Brr);
+        Brr++;
+    }
+
+    return iSum;
+}
+
+int main()
+{
+    int Arr[5];
+    int iRet = 0;
+
+    printf("Enter numbers : \n");
+
+    for(int iCnt = 0; iCnt < 5; iCnt++)
+    {
+        scanf("%d",&Arr[iCnt]);
+    }
+
+    iRet = Addition(Arr);
+
+    printf("sum of Array elements : %d",iRet);
+
+    return 0;
+}
