72ce3842dc0c6ed1a3a6538b931cf5a09a383ab9
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-11-27 01:36:56 +0530

    Create Program054_Passing_Array.c

---
+++ Program054_Passing_Array.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+// passing Array to the function & return Addition of its elements
+
+int Addition(int Brr[])
+{
+    int iSum = 0;
+
+    for(int iCnt = 0; iCnt < 5 ; iCnt++)
+    {
+        iSum = iSum + Brr[iCnt];
+    }
+
+    return iSum;
+}
+
+int main()
+{
+    int Arr[5];
+    int iRet = 0;
+
+    printf("Enter numbers : \n");
+
+    for(int iCnt = 0; iCnt < 5; iCnt++)
+    {
+        scanf("%d",&Arr[iCnt]);
+    }
+
+    iRet = Addition(Arr);
+
+    printf("sum of Array elements : %d",iRet);
+
+    return 0;
+}
