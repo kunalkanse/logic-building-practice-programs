896c63e4d7034ab9761ae58d1d3c5a8f09c0e60a
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-11-26 10:48:50 +0530

    Create Program042_Pattern_abcde.c

---
+++ Program042_Pattern_abcde.c
@@ -0,0 +1,33 @@
+
+// Input :      6
+
+// iCnt           1    2   3   4    5   6
+
+// Output :    a    b   c   d   e   f
+
+#include<stdio.h>
+
+void Display(int);
+
+int main()
+{
+    int iValue = 0;
+
+    printf("enter the number : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+    char ch = '\0';
+
+    for(iCnt = 1, ch = 'a' ; iCnt <= iNo ; iCnt++, ch++)
+    {
+        printf("%c\t",ch);
+    }
+}
