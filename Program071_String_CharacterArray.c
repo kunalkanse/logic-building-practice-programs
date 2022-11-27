c30b459d223b386435dcd4c8c7e9ce09a468c120
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-11-28 00:28:02 +0530

    Create Program071_String_CharacterArray.c

---
+++ Program071_String_CharacterArray.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+// in c & c++ String is considered as Character Array
+
+int main()
+{
+    char Arr[11] = "Marvellous";
+
+    printf("%c\n",Arr[0]);
+    printf("%c\n",Arr[5]);
+    printf("%c\n",Arr[9]);
+
+    return 0;
+}
