c4f9fcdb6bae9a932de51819bfb8b9ff99ea5f73
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-11-28 02:16:14 +0530

    Create Program078_StrlenX.c

---
+++ Program078_StrlenX.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+// create own function as strlenX to display length of string
+
+int strlenX(char *str)
+{
+    int iCnt = 0;
+
+    while(*str != '\0')
+    {
+        iCnt++;
+        str++;
+    }
+
+    return iCnt;
+}
+
+int main()
+{
+    char str[20];
+    int iRet = 0;
+
+    printf("Enter String : ");
+    scanf("%[^'\n']s",str);
+
+    iRet = strlenX(str);
+
+    printf("Length of string : %d",iRet);
+
+    return 0;
+}
