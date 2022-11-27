22321452c06cc4f35bf901f131b8801950641155
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-11-27 16:03:20 +0530

    Create Program066_CheckNo_WithoutFlag.c

---
+++ Program066_CheckNo_WithoutFlag.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+// check whether number is present in array or not
+// without using flag
+
+bool CheckNumber(int Brr[], int iSize)
+{
+    bool bRet = false;
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt < iSize; iCnt++)
+    {
+        if(Brr[iCnt] == 10)
+        {
+            break;
+        }
+    }
+
+    if(iCnt == iSize)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+
+}
+
+int main()
+{
+    int *Arr = NULL;
+    int iLength = 0;
+    bool bRet = 0;
+
+    printf("Enter size of Array : ");
+    scanf("%d",&iLength);
+
+    Arr = (int*) malloc (sizeof(int) * iLength);
+
+    for(int iCnt = 0; iCnt < iLength ; iCnt++)
+    {
+        scanf("%d",&Arr[iCnt]);
+    }
+     
+    bRet = CheckNumber(Arr,iLength);
+
+    if(bRet == true)
+    {
+        printf("Element present");
+    }
+    else
+    {
+        printf("Element not present");
+    }
+
+    free(Arr);
+    return 0;
+}
