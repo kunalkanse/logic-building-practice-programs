93176519c9d33caaaf747cecc2d7931a3b68ab58
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-11-28 02:41:24 +0530

    Create Program080_Ascii.c

---
+++ Program080_Ascii.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    char ch1 = 'A';
+    char ch2 = 'a';
+    char ch3 = '0';
+
+    printf("Ascii Value of ch1 : %d\n",ch1);
+    printf("Ascii Value of ch1 : %d\n",ch2);
+    printf("Ascii Value of ch1 : %d\n",ch3);
+
+    return 0;
+}
