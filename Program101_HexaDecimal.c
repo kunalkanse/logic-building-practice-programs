0a8fef139e6dad0d904c06c2c7b8ed1f8d674411
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-11-30 14:19:37 +0530

    Create Program101_HexaDecimal.c

---
+++ Program101_HexaDecimal.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int iValue = 0;
+    
+    printf("Enter number\n");
+    scanf("%d",&iValue);
+
+    printf("It's Decimal format is : %d\n",iValue);
+    printf("It's Hexadecimal format is : %x\n",iValue);
+    
+    return 0;
+}
