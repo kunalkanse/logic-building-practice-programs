fe2df674c4f919f54e609fce8d4da4cf04b1195f
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-12-02 10:51:48 +0530

    Create Program116_Array_input.java

---
+++ Program116_Array_input.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Program143_Array_input
+{
+    public static void main(String[] args) 
+    {
+        Scanner Sobj = new Scanner(System.in);
+
+        int iSize = 0,iCnt = 0;
+
+        System.out.println("Enter number of elements : ");
+        iSize = Sobj.nextInt();
+
+        int Arr[] = new int[iSize];
+
+        System.out.println("Enter Array Elements : ");
+        for(iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            Arr[iCnt] = Sobj.nextInt();
+        }
+
+        System.out.println("Entered Elements are : ");
+        for(iCnt = 0; iCnt<Arr.length; iCnt++)
+        {
+            System.out.println(Arr[iCnt] + " ");
+        }
+
+        Sobj.close();
+    }
+}
