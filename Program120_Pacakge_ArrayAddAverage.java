cf8ba22d39f19418b8b43dff3d107222f97511c3
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-12-02 12:10:39 +0530

    Create Program120_Pacakge_ArrayAddAverage.java

---
+++ Program120_Pacakge_ArrayAddAverage.java
@@ -0,0 +1,63 @@
+
+import java.util.*;
+import arrayXPackage.ArrayX;
+
+// This is industry level program for Average of Array Elements
+
+// inheritance , reusability
+class ArrayAddAverageX extends ArrayX
+{
+    public ArrayAddAverageX(int iSize)
+    {
+        super(iSize);
+    }
+
+    public int ArrayAdd()
+    {
+        int iSum = 0,iCnt = 0;
+
+        for(iCnt = 0; iCnt<Arr.length; iCnt++)
+        {
+            iSum = iSum + Arr[iCnt];
+        }
+
+        return iSum;
+    }
+
+    public float ArrayAverage()
+    {
+        int iSum = 0,iCnt = 0;
+
+        for(iCnt = 0; iCnt<Arr.length; iCnt++)
+        {
+            iSum = iSum + Arr[iCnt];
+        }
+
+        return (iSum/Arr.length);
+    }
+}
+
+class Program147_Pacakge_ArrayAddAverage
+{
+    public static void main(String arg[])
+    {
+        Scanner Sobj = new Scanner(System.in);
+        int iLength = 0;
+        float iRet = 0.0f;
+
+        System.out.println("Enter the Array size : ");
+        iLength = Sobj.nextInt();
+
+        ArrayAddAverageX ASobj = new ArrayAddAverageX(iLength);
+
+        ASobj.Accept();
+        ASobj.Display();
+
+        iRet = ASobj.ArrayAverage();
+
+        System.out.println("Average of Array Elements : "+iRet);
+
+    }
+}
+
+
