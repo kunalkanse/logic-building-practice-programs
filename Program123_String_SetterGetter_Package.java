5b26d77a0601dcad245055372489b5a84df51363
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-12-03 14:23:15 +0530

    Create Program123_String_SetterGetter_Package.java

---
+++ Program123_String_SetterGetter_Package.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+import stringXPackage.StringX;
+
+public class Program123_String_SetterGetter 
+{
+    public static void main(String[] args) 
+    {
+        StringX strobj = new StringX();
+        // This class is in stringXPackage
+
+        strobj.accept();
+        strobj.Display();
+    }
+}
+
