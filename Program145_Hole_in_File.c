7fa2a1c3fcd0c78d930eadf3064017665fd73d5a
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-12-07 10:55:24 +0530

    Create Program145_Hole_in_File.c

---
+++ Program145_Hole_in_File.c
@@ -0,0 +1,36 @@
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+// To creae hole in the file
+ 
+int main()
+{
+    int fd = 0;
+    char Data[10];
+
+    fd = open("FileHandling.txt" , O_RDWR);
+
+    if(fd == -1)
+    {
+        printf("Unable to open file\n");
+        return 0;
+    }
+
+    // 0    From starting position
+    // 1    From Current position
+    // 2    From end of the file
+
+    lseek(fd , 10, 2);
+
+    write(fd, " " ,1);
+
+    printf("\n");
+
+    close(fd);
+
+    return 0;
+}
