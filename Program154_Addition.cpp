a505d20bc695f91eeaec10dff68eec0e779ab055
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-12-07 22:28:13 +0530

    Create Program154_Addition.cpp

---
+++ Program154_Addition.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iNo1 = 0, iNo2 = 0, iRet = 0;
+
+    cout<<"Enter the first number : ";
+    cin>>iNo1;
+
+    cout<<"Enter the second number : ";
+    cin>>iNo2;
+
+    iRet = iNo1 + iNo2;
+
+    cout<<"Addition of Two Numbers : "<<iRet<<endl;
+
+    return 0;
+}
