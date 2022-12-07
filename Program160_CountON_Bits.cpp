92eb991ba4193a2440a53f657675cac86ff29e10
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-12-07 23:43:17 +0530

    Update Program160_CountON_Bits.cpp

--- Program160_CountON_Bits.cpp
+++ Program160_CountON_Bits.cpp
@@ -10,15 +10,13 @@ UINT CountONBit(UINT iNo)
     int iCnt = 0;
     UINT iMask = 0x00000001;
 
-    for(int i = 0; i < 32 ; i++)
+    for(int i = 0; i < 32 ; i++ , iMask = iMask << 1)
     {
 
         if((iNo & iMask) == iMask)
         {
             iCnt++;
         }
-
-        iMask = iMask << 1;
     }
 
     return iCnt;
