48caf85806a1106a1430b6ba76c859a614c5bf53
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-12-07 23:37:08 +0530

    Create Program160_CountON_Bits.cpp

---
+++ Program160_CountON_Bits.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+// Count On Bits
+
+typedef unsigned int UINT;
+
+UINT CountONBit(UINT iNo)
+{
+    int iCnt = 0;
+    UINT iMask = 0x00000001;
+
+    for(int i = 0; i < 32 ; i++)
+    {
+
+        if((iNo & iMask) == iMask)
+        {
+            iCnt++;
+        }
+
+        iMask = iMask << 1;
+    }
+
+    return iCnt;
+
+}
+
+int main()
+{
+    UINT iValue1 = 0,iRet = 0;
+
+    cout<<"Enter the number : ";
+    cin>>iValue1;
+
+    iRet = CountONBit(iValue1);
+
+    cout<<"Count of ON Bits is : "<<iRet<<endl;
+}
