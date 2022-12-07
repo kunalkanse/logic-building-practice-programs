f10f66f0de39030e70c1df37138ae3c7a613d992
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-12-07 22:26:42 +0530

    Create Program153_Cpp_HelloWorld.cpp

---
+++ Program153_Cpp_HelloWorld.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World"<<endl;
+    
+    // printf("Jay Ganesh...\n");
+    
+    // System.out.println("Jay Ganesh...");
+    
+    return 0;
+}
+
+// g++ Program184.cpp -o Myexe
+// ./Myexe
