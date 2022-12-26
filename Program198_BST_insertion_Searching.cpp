af7832cac0a290b020013fb361b736bab831ac5a
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-12-26 11:42:38 +0530

    Create Program198_BST_insertion_Searching.cpp

---
+++ Program198_BST_insertion_Searching.cpp
@@ -0,0 +1,132 @@
+
+// Program demonatrate insertion in BST & searching of element
+
+#include <iostream>
+using namespace std;
+
+// Structure decleration
+typedef struct node
+{
+    int data;
+    struct node *lChild;
+    struct node *rChild;
+
+} NODE, *PNODE, **PPNODE;
+
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+
+void insert(PPNODE Head, int no)
+{
+    PNODE newn = new NODE;
+    PNODE temp = *Head; // new node
+
+    newn->data = no;
+    newn->lChild = NULL;
+    newn->rChild = NULL;
+
+    if (*Head == NULL) // if tree is empty
+    {
+        *Head = newn;
+    }
+    else
+    {
+        while (1) // uncondition loop , while(true) , for( ; ; )
+        {
+            if ((temp->data) == no)
+            {
+                cout << "Duplication Data\n";
+
+                delete newn; // if duplicate , Deallocate
+                break;
+            }
+            else if (no < (temp->data)) // if Data is small
+            {
+                if ((temp->lChild) == NULL)
+                {
+                    temp->lChild = newn;
+                    break;
+                }
+
+                temp = temp->lChild;
+            }
+            else if (no > (temp->data)) // if Data is large
+            {
+                if (temp->rChild == NULL)
+                {
+                    temp->rChild = newn;
+                    break;
+                }
+
+                temp = temp->rChild;
+            }
+        }
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+bool search(PNODE Head , int no)
+{
+    bool flag = false;
+
+    if(Head == NULL)    // if treee is empty
+    {
+        return false;
+    }
+    else    // tree contains atleast one node
+    {
+        while(Head != NULL) // while(1) not works if data is not present
+        {
+            if((Head->data) == no)
+            {
+                flag = true;
+                break;
+            }
+            else if(no < (Head->data))  // if Data is small
+            {
+                Head = Head->lChild;
+            }
+            else if(no > (Head->data))  // if Data is large
+            {
+                Head = Head->rChild;
+            }
+        }
+
+        return flag;
+    }
+}
+
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+int main()
+{
+    PNODE first = NULL;
+    int no = 0;
+    bool bRet = false;
+ 
+    insert(&first, 51);
+    insert(&first, 21);
+    insert(&first, 101);
+    insert(&first, 75);
+    insert(&first, 36);
+    insert(&first, 62);
+
+    cout<<"Enter number to search : ";
+    cin>>no;
+
+    bRet = search(first , no);
+
+    if(bRet == true)
+    {
+        cout<<"\nElement is present\n";
+    }
+    else
+    {
+        cout<<"\nElement is not present\n";
+    }
+
+
+    return 0;
+}
