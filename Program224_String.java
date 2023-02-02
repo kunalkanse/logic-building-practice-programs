d1cc25f8bca92ca982cc091226e615d30da79cb3
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2023-02-02 23:40:29 +0530

    Create Program224_String.java

---
+++ Program224_String.java
@@ -0,0 +1,17 @@
+
+
+import java.util.*;
+
+class Program224_String
+{
+    public static void main(String[] args) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the String : ");
+        String str = sobj.nextLine();
+
+        System.out.println("Entered String is : " +str);
+        System.out.println("Lenght of String is : " +str.length());
+    }
+}
