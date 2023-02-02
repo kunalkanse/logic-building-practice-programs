340814c967d3c92344f866d7a1d9e0ece63ee9d2
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2023-02-02 23:45:22 +0530

    Create Program230_String_trim_replaceAll_split.java

---
+++ Program230_String_trim_replaceAll_split.java
@@ -0,0 +1,33 @@
+
+// replaceAll() removes all extra white spaces of starting & ending of string
+// in replaceAll regular expression (Regex) is used 
+// nested function calll
+
+//  split() : tokenize the string
+
+import java.util.*;
+
+class Program230_String_trim_replaceAll_split
+{
+    public static void main(String[] args) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the String : ");
+        String str = sobj.nextLine();
+
+        System.out.println("Entered String is : " +str);
+        System.out.println("Length of String is : " +str.length());
+
+        String str1_new = str.trim().replaceAll("[ ]{3,}"," ");
+
+        System.out.println("new String : " +str1_new);
+        System.out.println("Length of trim String is : " +str1_new.length());
+
+        
+        String Arr[] = str1_new.split(" ");
+
+        System.out.println("Number of words in string : "+Arr.length);
+
+    }
+}
